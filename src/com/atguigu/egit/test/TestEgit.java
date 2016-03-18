84287fa4b53303a23b64dd10d8a32381d2fa95fa
qwt311 <qwt311@126.com>
2016-03-18 22:08:48 +0800

    github update 01

--- src/com/atguigu/egit/test/TestEgit.java
+++ src/com/atguigu/egit/test/TestEgit.java
@@ -5,6 +5,7 @@ public class TestEgit {
 	public static void main(String[] args) {
 		System.out.println("111111111111111");
 		System.out.println("222222222222222");
+		System.out.println("github update 01");
 	}
 
 }
